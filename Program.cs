1faec0cc6f29fd87b4a863ee416c7d414aa86362
Klebervini49 <klebervini49@gmail.com>
2022-12-30 18:55:32 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("Olá");
+        }
+
+        static void Menu()
+        {
+
+        }
+    }
+}
